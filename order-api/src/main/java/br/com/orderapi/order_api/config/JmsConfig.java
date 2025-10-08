08ed4f2874f8ff12568d127e92f14d90ab8541b4
Wesley Werikis <wesleywerikis@outlook.com>
2025-10-08 17:56:48 -0300

    feat(config): add JMS configuration for order queue and JSON message converter

---
+++ order-api/src/main/java/br/com/orderapi/order_api/config/JmsConfig.java
@@ -0,0 +1,18 @@
+package br.com.orderapi.order_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    public static final String QUEUE_ORDERS_NEW = "orders.new";
+
+    public MappingJackson2MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
