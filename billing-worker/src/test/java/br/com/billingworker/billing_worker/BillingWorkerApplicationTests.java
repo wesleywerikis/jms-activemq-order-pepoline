275f1d5d592ecf200f6a4e7d53c04f6d1ef09541
Wesley Werikis <wesleywerikis@outlook.com>
2025-10-09 16:07:26 -0300

    test(billing-worker-test): Add basic application context load test
     - Created 'BillingWorkerApplicationTests' to verify Spring Boot context initialization
     - Ensures JMS configuration and component scanning load without errors
     - Provides a base structure for future integration tests in billing-worker

---
+++ billing-worker/src/test/java/br/com/billingworker/billing_worker/BillingWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.billingworker.billing_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
