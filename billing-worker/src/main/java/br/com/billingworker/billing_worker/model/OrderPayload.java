ce2cad580f49a3c826aa41d94882095b3deff1e8
Wesley Werikis <wesleywerikis@outlook.com>
2025-10-09 16:17:50 -0300

    feat(billing-worker-model): Add OrderPayload entity with validation and Lombok support
     - Created OrderPayload class in package .br.com.billingworker.billing_worker.model
     - Added fields: orderId, customerEmail, item, quantity
     - Applied validation annotations: @NotBlank and @Min(1)
     - Enabled Lombok annotations: @Data, @NoArgsConstructor, @AllArgsConstructor, @Builder
     - Represents the base order message structure consumed from the queue

---
+++ billing-worker/src/main/java/br/com/billingworker/billing_worker/model/OrderPayload.java
@@ -0,0 +1,27 @@
+package br.com.billingworker.billing_worker.model;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderPayload {
+        
+    @NotBlank
+    private String orderId;
+
+    @NotBlank
+    private String customerEmail;
+
+    @NotBlank
+    private String item;
+    
+    @Min(1)
+    private int quantity;
+}
