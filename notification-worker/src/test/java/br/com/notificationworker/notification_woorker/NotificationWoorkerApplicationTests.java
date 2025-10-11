9cb52abde2473794dffb9b11589345a7bc7737f3
Wesley Werikis <wesleywerikis@outlook.com>
2025-10-11 17:02:52 -0300

    chore(notification-worker): Rename module and fix package naming
     - Rename folter 'notification-wooker > notification-worker' to correct double 'oo' type
     - Updated all Java package declarations from 'br.com.notificationworker.notification_woorker' > 'br.com.notificationworker.notification_worker'
     - Fixed import statements accordingly (config, listener, model)
     - Renamed main class to 'NotificationWorkerApplication' for consistency
     - Verifild build with '\mvnw clean package\' and confirmed app starts successfully

--- notification-worker/src/test/java/br/com/notificationworker/notification_woorker/NotificationWoorkerApplicationTests.java
+++ notification-worker/src/test/java/br/com/notificationworker/notification_woorker/NotificationWoorkerApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.notificationworker.notification_woorker;
+package br.com.notificationworker.notification_worker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
